7944411dd07a29440cf55cd6ebc8a29a28b40d38
Francisco Martin Rico <fmartin@gsyc.urjc.es>
2019-09-25 09:56:50 +0200

    Callbacks are pointer to functions
    Signed-off-by: Francisco Martin Rico <fmartin@gsyc.urjc.es>

--- rclobjc_examples/src/ROSAddTwoIntsClientAsync.m
+++ rclobjc_examples/src/ROSAddTwoIntsClientAsync.m
@@ -18,6 +18,12 @@
 #import <ROS_example_interfaces/srv/AddTwoInts.h>
 #import <rclobjc/ROSRCLObjC.h>
 
+void MyCallback(ROS_example_interfaces_srv_AddTwoInts_Response *response)
+{
+  NSLog(@"Incoming response\n");
+  NSLog(@"sum: %lld\n", [response sum]);
+}
+
 int main() {
   [ROSRCLObjC rclInit];
   ROSNode *node = [ROSRCLObjC createNode:@"add_two_ints_client"];
@@ -33,10 +39,7 @@ int main() {
 
   [client
       sendRequest:
-          request:^(ROS_example_interfaces_srv_AddTwoInts_Response *response) {
-            NSLog(@"Incoming response\n");
-            NSLog(@"sum: %lld\n", [response sum]);
-          }];
+          request:MyCallback];
 
   while ([ROSRCLObjC ok]) {
     [ROSRCLObjC spinOnce:node];
